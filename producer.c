2edb1327522b7922cff370067ae8017da397f699
Rushan Yogaratnam <rushan.dev@outlook.com>
2014-07-22 22:51:38 -0400

    updated comments

--- producer.c
+++ producer.c
@@ -1,10 +1,13 @@
-
 /*
  * producer.c
  * ECE254 Group 01
  * By : Rushan Yogaratnam and Ameen Patel
  * University of Waterloo Computer Engineering
  * Spring 2014
+ *
+ * Producer Consumer Process Implementation:
+ * This is the producer child process that
+ * the parent process forks.
  *
  */
 #include <mqueue.h>
@@ -21,36 +24,41 @@
 
 int main(int argc, char **argv) {
 
+	//this is the assigned producer id, between 0 and P-1.
+	int pid = atoi(argv[2]);
 
-  	int pid = atoi(argv[2]);
+	//the number of producer, P
 	int producer_count = atoi(argv[3]);
+
+	//number of messages to produce
 	int production_count = atoi(argv[1]);
 
-
+	//attempt to open the queue and perform error handling
 	mqd_t queue_descriptor;
 	queue_descriptor = mq_open(queue_name, O_RDWR);
 
 	if (queue_descriptor == -1) {
-		printf("error opening queue in producer %s\n",strerror(errno));
+		printf("error opening queue in producer %s\n", strerror(errno));
 		return 1;
 	}
 
-         int i;
-	 for(i = pid ; i<production_count;i +=producer_count)
-	 {
-	    int message = i;
-	    if (mq_send(queue_descriptor, (char*) &message, sizeof(int), 0) == -1)
-	    {
-		printf("P: pid %d send failed %s \n",getpid(), strerror(errno));
-		return 1;
-	    }
-	 }
+	//produce only the set of elements that satisfy i%num_producers = pid.
+	int i;
+	for (i = pid; i < production_count; i += producer_count) {
 
+		//send a message to the queue, blocks if queue is full.
+		int message = i;
+		if (mq_send(queue_descriptor, (char*) &message, sizeof(int), 0) == -1) {
+			printf("P: pid %d send failed %s \n", getpid(), strerror(errno));
+			return 1;
+		}
+	}
+
+	//close the queue
 	if (mq_close(queue_descriptor) == -1) {
 		perror("mq_close failed");
 		exit(2);
 	}
-
 
 	return 0;
 
--- producerConsumerParent.c
+++ producerConsumerParent.c
@@ -1,9 +1,14 @@
 /*
- * Producer.c
+ * producerConsumerParent.c
  * ECE254 Group 01
  * By : Rushan Yogaratnam and Ameen Patel
  * University of Waterloo Computer Engineering
  * Spring 2014
+ *
+ * Producer Consumer Process Implementation:
+ * This is the parent process which fork's and
+ * exec's the producer and consumer processes
+ * as child processes.
  *
  */
 
@@ -29,12 +34,14 @@ int main(int argc, char **argv) {
 	int num_producers;
 	int num_consumers;
 
+	//validate the command line arguments
 	if (process_arguments(argc, argv, &queue_size, &message_count,
 			&num_producers, &num_consumers)) {
 		printf("Invalid arguments\n");
 		return 1;
 	}
 
+	//set queue attributes
 	struct mq_attr queue_attributes;
 	queue_attributes.mq_maxmsg = queue_size;
 	queue_attributes.mq_msgsize = sizeof(int);
@@ -43,6 +50,7 @@ int main(int argc, char **argv) {
 	mqd_t queue_descriptor;
 	mode_t permissions = S_IRUSR | S_IWUSR;
 
+	//attempt to open queue and perform error checking
 	queue_descriptor = mq_open(queue_name, O_RDWR | O_CREAT, permissions,
 			&queue_attributes);
 
@@ -51,7 +59,9 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-
+	//this semaphore is used as a counting semaphore
+	//it is used by the consumers to determine wether or not
+	//they should continue consumption
 	sem_t *consumer_sem = sem_open("consumer_sem2", O_RDWR | O_CREAT,
 			permissions, message_count);
 
@@ -59,32 +69,35 @@ int main(int argc, char **argv) {
 		printf("failed to create consumer semaphore\n");
 	}
 
+	//get time before first fork
+	double time_before_first_fork = get_time_in_seconds();
 
-    double time_before_first_fork = get_time_in_seconds();
-
+	//spawn producer processes
 	int i;
 	for (i = 0; i < num_producers; ++i) {
 
-	    spawn_child("./producer", argv,i,num_producers);
+		spawn_child("./producer", argv, i, num_producers);
 	}
 
+	//spawn consumer processes
 	int j;
 	for (j = 0; j < num_consumers; ++j) {
 
-	    spawn_child("./consumer", argv,j,num_consumers);
+		spawn_child("./consumer", argv, j, num_consumers);
 	}
-
 
 	int status, pid;
 
-	//Wait on all child processes to complete
-	while ((pid = wait(&status)) != -1){}
+	//busy loop and wait for all of the child
+	//processes to complete execution.
 
+	while ((pid = wait(&status)) != -1) {
+	}
 
 	double time_after_last_consumed = get_time_in_seconds();
 
 	double execution_time = time_after_last_consumed - time_before_first_fork;
-	printf("System execution time: %f seconds\n",execution_time);
+	printf("System execution time: %f seconds\n", execution_time);
 
 	//Tidy up queues and semaphores
 
@@ -105,7 +118,6 @@ int main(int argc, char **argv) {
 		exit(2);
 	}
 
-
 	if (sem_unlink("consumer_sem2") == -1) {
 		perror("failed to unlink consumer semaphore");
 		exit(3);
@@ -115,15 +127,13 @@ int main(int argc, char **argv) {
 
 }
 
-int spawn_child(char* program, char **arg_list,int p_id,int childCount) {
-
+int spawn_child(char* program, char **arg_list, int p_id, int childCount) {
 
 	arg_list[0] = program;
 
-	char pid [15];
-	sprintf(pid,"%d",p_id);
+	char pid[15];
+	sprintf(pid, "%d", p_id);
 	arg_list[2] = &pid;
-
 
 	pid_t child_pid;
 	child_pid = fork();
